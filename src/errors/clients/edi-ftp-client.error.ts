b0fa132a87a35f32f9c3ee3fd74fae5df9d3ee1a
Victor Cazotti <victor.cazotti@enviabybus.com.br>
2022-03-21 11:11:48 -0300

    [edi-proceda] Remove edi-ftp client

--- src/errors/clients/edi-ftp-client.error.ts
+++
@@ -1,7 +0,0 @@
-export class EdiFtpClientError extends Error {}
-
-export class EdiFtpClientFileNotFoundError extends EdiFtpClientError {
-  constructor(fileName: string) {
-    super(`FTP file not found: ${fileName}`);
-  }
-}
