97d9b58730a1e1da6e7a1c4c7c50c59fdb660915
Victor Cazotti <victor.cazotti@enviabybus.com.br>
2022-07-04 13:55:27 -0300

    [edi-proceda] Corrige exportação do padrão ocoren proceda 3.1

--- lib/schemas/ocoren/proceda-3-1/index.ts
+++ lib/schemas/ocoren/proceda-3-1/index.ts
@@ -1,8 +1,16 @@
 import { EdiSchema } from '../../..';
+import cabecalhoDeDocumentoSchema from './340-cabecalho-de-documento.schema';
 import cabecalhoDeIntercambioSchema from './000-cabecalho-de-intercambio.schema';
+import complementoDeOcorrencia from './343-complemento-de-ocorrencia.schema';
+import dadosDaTransportadora from './341-dados-da-transportadora.schema';
+import ocorrenciaNaEntrega from './342-ocorrencia-na-entrega.schema';
 
 const proceda_3_1: EdiSchema[] = [
   cabecalhoDeIntercambioSchema,
+  cabecalhoDeDocumentoSchema,
+  dadosDaTransportadora,
+  ocorrenciaNaEntrega,
+  complementoDeOcorrencia,
 ];
 
 export default proceda_3_1;
